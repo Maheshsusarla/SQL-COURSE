fd3be2b799403edd0e85ab2c2525989e5220b1c6
Mahesh <maheshsusarla5@gmail.com>
2026-05-08 14:23:57 +0530

    Add files via upload

---
+++ sql1.sql
@@ -0,0 +1,27 @@
+-- what is sql :
+/*
+SQL stands for Structured Query Language. 
+It’s a programming language used to interact with databases—especially relational databases.
+In simple terms, SQL lets you store, retrieve, and manage data.
+The main creators were:
+Donald D. Chamberlin
+Raymond F. Boyce
+Their design was strongly inspired by the relational database theory proposed by:
+Edgar F. Codd:1970: The relational database model was proposed by Edgar F. Codd 
+*/
+-- what is my sql 
+/*
+MySQL is a relational database management system (RDBMS)
+—a software used to store, organize, and retrieve data using SQL.
+It is one of the most popular implementations of SQL.
+MySQL is a software system that uses SQL to help you store and manage data in databases efficiently.
+*/
+-- what is databse
+/*
+A database is an organized collection of data that is stored electronically so it can be easily accessed, managed, and updated.
+Types of databases:
+Relational databases:MySQL
+PostgreSQL
+Microsoft SQL Server
+Non-relational databases (NoSQL) :1. MongoDB
+*/
